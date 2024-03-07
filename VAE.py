298c02c4e6827ff717b7c24ba521a31e5b3d1fbc
George Abu Daoud <abudauodg@gmail.com>
2024-03-07 15:53:46 -0500

    created VAE file

---
+++ VAE.py
@@ -0,0 +1,113 @@
+import torch
+import numpy as np
+import torch.nn as nn
+from torch.optim import Adam
+import matplotlib.pyplot as plt
+from torchvision.datasets import MNIST
+from torch.utils.data import DataLoader
+import torchvision.transforms as transforms
+from mpl_toolkits.axes_grid1 import ImageGrid
+from torchvision.utils import save_image, make_grid
+
+# create a transofrm to apply to each datapoint
+transform = transforms.Compose([transforms.ToTensor()])
+
+# download the MNIST datasets
+path = '~/datasets'
+train_dataset = MNIST(path, transform=transform, download=True)
+test_dataset  = MNIST(path, transform=transform, download=True)
+
+# create train and test dataloaders
+batch_size = 100
+train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+class VAE(nn.Module):
+
+    def __init__(self, input_dim=784, hidden_dim=400, latent_dim=200, device=device):
+        super(VAE, self).__init__()
+
+        # encoder
+        self.encoder = nn.Sequential(
+            nn.Linear(input_dim, hidden_dim),
+            nn.LeakyReLU(0.2),
+            nn.Linear(hidden_dim, latent_dim),
+            nn.LeakyReLU(0.2)
+            )
+        
+        # latent mean and variance 
+        self.mean_layer = nn.Linear(latent_dim, 2)
+        self.logvar_layer = nn.Linear(latent_dim, 2)
+        
+        # decoder
+        self.decoder = nn.Sequential(
+            nn.Linear(2, latent_dim),
+            nn.LeakyReLU(0.2),
+            nn.Linear(latent_dim, hidden_dim),
+            nn.LeakyReLU(0.2),
+            nn.Linear(hidden_dim, input_dim),
+            nn.Sigmoid()
+            )
+     
+    def encode(self, x):
+        x = self.encoder(x)
+        mean, logvar = self.mean_layer(x), self.logvar_layer(x)
+        return mean, logvar
+
+    def reparameterization(self, mean, var):
+        epsilon = torch.randn_like(var).to(device)      
+        z = mean + var*epsilon
+        return z
+
+    def decode(self, x):
+        return self.decoder(x)
+
+    def forward(self, x):
+        mean, logvar = self.encode(x)
+        z = self.reparameterization(mean, logvar)
+        x_hat = self.decode(z)
+        return x_hat, mean, logvar
+    
+def loss_function(x, x_hat, mean, log_var):
+    reproduction_loss = nn.functional.binary_cross_entropy(x_hat, x, reduction='sum')
+    KLD = - 0.5 * torch.sum(1+ log_var - mean.pow(2) - log_var.exp())
+
+    return reproduction_loss + KLD
+
+def train(model, optimizer, epochs, device):
+    model.train()
+    for epoch in range(epochs):
+        overall_loss = 0
+        for batch_idx, (x, _) in enumerate(train_loader):
+            x = x.view(batch_size, x.dim(), -1).to(device)
+
+            optimizer.zero_grad()
+
+            x_hat, mean, log_var = model(x)
+            loss = loss_function(x, x_hat, mean, log_var)
+            
+            overall_loss += loss.item()
+            
+            loss.backward()
+            optimizer.step()
+
+        print("\tEpoch", epoch + 1, "\tAverage Loss: ", overall_loss/(batch_idx*batch_size))
+    return overall_loss
+
+def generate_digit(mean, var):
+    z_sample = torch.tensor([[mean, var]], dtype=torch.float).to(device)
+    x_decoded = model.decode(z_sample)
+    digit = x_decoded.detach().cpu().reshape(28, 28) # reshape vector to 2d array
+    plt.imshow(digit, cmap='gray')
+    plt.axis('off')
+    plt.show()
+    
+if __name__ == '__main__':
+    epochs = 50
+
+    model = VAE().to(device)
+    optimizer = Adam(model.parameters(), lr=1e-3)
+
+    train(model, optimizer, epochs, device)
