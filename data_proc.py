fb04d7d0472dd6a9c31e41bb93056d722377b922
George Abu Daoud <abudauodg@gmail.com>
2024-03-25 14:19:13 -0400

    generated pdb and changed data_proc funcs

---
+++ data_proc.py
@@ -0,0 +1,28 @@
+import os
+import mdtraj
+
+dft_path = 'DFT_results'
+files = os.listdir(dft_path)
+
+def get_molecule(mol):
+    if not mol in files:
+        raise KeyError(f'no molecule {mol} in folder')
+    
+    file_path = os.path.join(dft_path, f'{mol}_DFT_trj.xyz')
+    return file_path
+
+def get_molecules():
+    mols = []
+    for mol in files:
+        if mol != 'extract_data.sh':
+            mols.append(mol)
+
+    return mols
+
+def get_trajectory(mol):
+    if not mol in files:
+        raise KeyError(f'no molecule {mol} in folder')
+    
+    file_path = os.path.join(dft_path, f'{mol}/{mol}_DFT_topology.pdb')
+    print(file_path)
+    return mdtraj.load(file_path)
